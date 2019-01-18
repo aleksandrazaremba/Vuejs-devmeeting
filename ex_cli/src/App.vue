dacd5ca63751723db6a799968cac0acc82b192f2
aleksandrazaremba <aleksandrazarem@gmail.com>
2019-01-18 00:10:11 +0000

    3.1 [Added components]

--- ex_cli/src/App.vue
+++ ex_cli/src/App.vue
@@ -1,20 +1,27 @@
 <template>
   <div id="app">
+    <product-list :products="products"></product-list>
     <form @submit.prevent="onSubmit()">
-      <input name="newProduct" v-model="newProductName" v-validate="'required|min:3'">
+      <input name="product" v-model="newProductName" v-validate="'required|min:3'">
       <button>Add</button>
       {{ msg }}
+      <div v-show="errors.has('product')">{{ errors.first('product') }}</div>
     </form>
-
     <ul>
       <li v-for="p in products" :key="p.id">{{ p.name }}</li>
     </ul>
+    <p v-if="!products.length">No products!</p>
   </div>
 </template>
 
 <script>
+import ProductList from "./components/ProductList";
+
 export default {
-  name: "app",
+  name: "App",
+  components: {
+    ProductList
+  },
   data() {
     return {
       msg: "Add your product name",
@@ -28,18 +35,22 @@ export default {
           name: "Apple"
         }
       ],
-      newProductID: 2,
+      newProductId: 2,
       newProductName: ""
     };
   },
   methods: {
     onSubmit() {
-      this.products.push({
-        id: this.newProductId++,
-        name: this.newProductName
+      this.$validator.validateAll().then(result => {
+        if (result) {
+          this.products.push({
+            id: this.newProductId++,
+            name: this.newProductName
+          });
+          this.newProductName = "";
+          this.$validator.reset();
+        }
       });
-      this.newProductName = "";
-      this.$validator.reset();
     }
   }
 };
@@ -50,7 +61,6 @@ export default {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  // text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ ex_cli/src/components/ProductList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="p in products" :key="p.id">{{ p.name }}</li>
+    </ul>
+    <p v-if="!products.length">No products!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductList",
+  props: {
+    products: {
+      type: Array
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
